0247f6107dad497de964eb443783e796678d6141
LQC-labs <mahogani2018@gmail.com>
2026-09-13 19:06:57 +0900

    Label the testnet token as tLQC in the UI

--- dex/app/config.js
+++ dex/app/config.js
@@ -22,6 +22,6 @@ window.LQC_FLOW_CONFIG = Object.freeze({
   tokens: [
     { symbol: "BNB", name: "BNB", address: "native", decimals: 18 },
     { symbol: "WBNB", name: "Wrapped BNB", address: "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd", decimals: 18 },
-    { symbol: "LQC", name: "LQC Test Token", address: "0x84a30A66cFCbb15453C83204B7e6eC436a0718Fc", decimals: 18 }
+    { symbol: "LQC", displaySymbol: "tLQC", name: "LQC Test Token", address: "0x84a30A66cFCbb15453C83204B7e6eC436a0718Fc", decimals: 18 }
   ]
 });
